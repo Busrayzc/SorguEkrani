be824f315e3798c3f1009565cd80879a10619724
Busrayzc <116655150+Busrayzc@users.noreply.github.com>
2024-09-30 09:32:08 +0300

    Add files via upload

---
+++ SorguEkran/SorguEkran/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace SorguEkran
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.txtTckn = new System.Windows.Forms.TextBox();
+            this.lblTckn = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.groupBox1.Controls.Add(this.btnTemizle);
+            this.groupBox1.Controls.Add(this.btnAra);
+            this.groupBox1.Controls.Add(this.dtEnd);
+            this.groupBox1.Controls.Add(this.lblEnd);
+            this.groupBox1.Controls.Add(this.dtStart);
+            this.groupBox1.Controls.Add(this.lblStart);
+            this.groupBox1.Controls.Add(this.txtTckn);
+            this.groupBox1.Controls.Add(this.lblTckn);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(0, -1);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(986, 364);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ödeme Sorgulama Ekranı";
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnTemizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTemizle.ForeColor = System.Drawing.Color.Black;
+            this.btnTemizle.Location = new System.Drawing.Point(607, 171);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(219, 51);
+            this.btnTemizle.TabIndex = 7;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnAra
+            // 
+            this.btnAra.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnAra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAra.ForeColor = System.Drawing.Color.Black;
+            this.btnAra.Location = new System.Drawing.Point(607, 93);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(219, 51);
+            this.btnAra.TabIndex = 6;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = false;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.CalendarMonthBackground = System.Drawing.Color.White;
+            this.dtEnd.CustomFormat = "yyyy-MM-dd";
+            this.dtEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtEnd.Location = new System.Drawing.Point(199, 192);
+            this.dtEnd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.Size = new System.Drawing.Size(298, 30);
+            this.dtEnd.TabIndex = 5;
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEnd.ForeColor = System.Drawing.Color.Black;
+            this.lblEnd.Location = new System.Drawing.Point(66, 197);
+            this.lblEnd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(121, 25);
+            this.lblEnd.TabIndex = 4;
+            this.lblEnd.Text = "Bitiş Tarihi:";
+            // 
+            // dtStart
+            // 
+            this.dtStart.CalendarMonthBackground = System.Drawing.Color.White;
+            this.dtStart.CustomFormat = "yyyy-MM-dd";
+            this.dtStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtStart.Location = new System.Drawing.Point(199, 136);
+            this.dtStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(298, 30);
+            this.dtStart.TabIndex = 3;
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblStart.ForeColor = System.Drawing.Color.Black;
+            this.lblStart.Location = new System.Drawing.Point(17, 141);
+            this.lblStart.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(174, 25);
+            this.lblStart.TabIndex = 2;
+            this.lblStart.Text = "Başlangıç Tarihi:";
+            // 
+            // txtTckn
+            // 
+            this.txtTckn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTckn.Location = new System.Drawing.Point(199, 90);
+            this.txtTckn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTckn.Name = "txtTckn";
+            this.txtTckn.Size = new System.Drawing.Size(298, 30);
+            this.txtTckn.TabIndex = 1;
+            // 
+            // lblTckn
+            // 
+            this.lblTckn.AutoSize = true;
+            this.lblTckn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTckn.ForeColor = System.Drawing.Color.Black;
+            this.lblTckn.Location = new System.Drawing.Point(100, 93);
+            this.lblTckn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTckn.Name = "lblTckn";
+            this.lblTckn.Size = new System.Drawing.Size(79, 25);
+            this.lblTckn.TabIndex = 0;
+            this.lblTckn.Text = "TCKN:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightSkyBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Snow;
+            this.dataGridView1.Location = new System.Drawing.Point(-24, 371);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1612, 300);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(1581, 667);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.TextBox txtTckn;
+        private System.Windows.Forms.Label lblTckn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnAra;
+    }
+}
+
---
+++ SorguEkran/SorguEkran/Form1.cs
@@ -0,0 +1,145 @@
+﻿using SorguEkran;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Data.SqlClient.SqlException;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SorguEkran
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.CellDoubleClick += DataGridView1_CellDoubleClick;
+            txtTckn.KeyPress += txtTckn_KeyPress;
+        }
+
+        SqlConnection baglanti = new SqlConnection("data source=localhost;initial catalog=SorguEkranDb;integrated security=True;trustservercertificate=True;");
+        SorguEkranDbEntities db = new SorguEkranDbEntities();
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            var gunFark = dtEnd.Value.Day - dtStart.Value.Day;
+            if (gunFark <= 7)
+            {
+
+            }  
+            else
+            {
+                MessageBox.Show("Tarih aralığı 1 hafta ile kısıtlanmalıdır.");
+            }
+            
+            // TCKN kontrolü
+                if (!string.IsNullOrEmpty(txtTckn.Text) && txtTckn.Text.Length != 11)
+            {
+                MessageBox.Show("TCKN 11 haneli olmalıdır.");
+                return;
+            }
+
+            // Kullanıcının TCKN girip girmediğini kontrol et
+            if (!string.IsNullOrEmpty(txtTckn.Text))
+            {
+                if (dtStart.Value.Date != DateTime.Now.Date && dtEnd.Value.Date != DateTime.Now.Date)
+                {
+                    var odemeListesi1 = db.TblOdeme.Where(x => x.TCKN==txtTckn.Text && x.Başlangıç_Tarihi>= dtStart.Value.Date && x.Bitiş_Tarihi<= dtEnd.Value.Date).ToList();  
+                    if (odemeListesi1.Count != 0)
+                    {
+                        dataGridView1.DataSource = odemeListesi1;
+                        dataGridView1.Columns[13].Visible = false;
+                        dataGridView1.Columns[14].Visible = false;
+                        MessageBox.Show("Sonuçlar listeleniyor.");
+                    }
+                }
+                else
+                {
+                    // TCKN'ye ait tüm kayıtları al
+                    var odemeListesi = db.TblOdeme
+                        .Where(x => x.TCKN == txtTckn.Text)
+                        .ToList();
+
+                    if (odemeListesi.Count != 0)
+                    {
+                        dataGridView1.DataSource = odemeListesi;
+                        dataGridView1.Columns[13].Visible = false;
+                        dataGridView1.Columns[14].Visible = false;
+                        MessageBox.Show("Sonuçlar listeleniyor.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bu TCKN'ye ait kayıt bulunmamaktadır.");
+                    }
+                }
+               
+                    
+            }
+            else
+            {
+                var odemeListesi2 = db.TblOdeme.Where(x=> x.Başlangıç_Tarihi == dtStart.Value.Date && x.Bitiş_Tarihi == dtEnd.Value.Date).ToList(); 
+                if(odemeListesi2.Count != 0)
+                {
+                    dataGridView1.DataSource= odemeListesi2;
+                    dataGridView1.Columns[13].Visible = false;
+                    dataGridView1.Columns[14].Visible = false;
+                    MessageBox.Show("Sonuçlar listeleniyor.");
+                }
+                
+            }
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            txtTckn.Text = string.Empty;
+            dtStart.Value = DateTime.Now;
+            dtEnd.Value = DateTime.Now;
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            MessageBox.Show("Alanlar temizlendi.");
+        }
+
+        private void txtTckn_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                MessageBox.Show("Lütfen sadece rakam giriniz.", "Geçersiz Giriş", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                e.Handled = true; 
+            }
+        }
+        private void DataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            if (e.RowIndex >= 0)
+            {
+                
+                int durumKodSutunu = 9; 
+
+                
+                if (e.ColumnIndex == durumKodSutunu)
+                {
+                    var paymentDetails = dataGridView1.Rows[e.RowIndex].DataBoundItem as TblOdeme; 
+                    if (paymentDetails != null)
+                    {
+                        
+                        MessageBox.Show($"Ödeme Bilgileri:\n\n" +
+                                        $"TCKN: {paymentDetails.TCKN}\n" +
+                                        $"Başlangıç Tarihi: {paymentDetails.Başlangıç_Tarihi}\n" +
+                                        $"Bitiş Tarihi: {paymentDetails.Bitiş_Tarihi}\n" +
+                                        $"Ödeme Tarihi: {paymentDetails.Ödeme_Tarihi}\n" +
+                                        $"Alacaklı IBAN: {paymentDetails.Alacaklı_Iban}\n" +
+                                        $"Borçlu IBAN: {paymentDetails.Borçlu_İban}\n" +
+                                        $"Döviz Kodu: {paymentDetails.Döviz_Kodu}\n" +
+                                        $"Tutar: {paymentDetails.Tutar}\n" +
+                                        $"Açıklama: {paymentDetails.Açıklama}\n",
+                                        "Ödeme Detayları", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+    }
+}
